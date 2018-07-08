6ee8743089c3818e707678498544db9026b9c274
daniel-salgado <daniel@salgadobr.com>
2018-07-09 00:27:05 +1200

    Improved tests but cannot perform Delete_Product_ShouldReturnOk properly

--- WebApiBase.Tests/UnitTestProduct.cs
+++ WebApiBase.Tests/UnitTestProduct.cs
@@ -4,6 +4,7 @@ using System.Web.Http.Results;
 using WebApiBase.Controllers;
 using WebApiBase.Core;
 using WebApiBase.Core.Dtos;
+using WebApiBase.Core.Models;
 using WebApiBase.Core.Repositories;
 
 namespace WebApiBase.Tests
@@ -23,11 +24,9 @@ namespace WebApiBase.Tests
 
             // Initialize mock UnitOfWork.
             var mockUnitOfWork = new Mock<IUnitOfWork>();
-
             mockUnitOfWork.SetupGet(m => m.Products).Returns(_mockRepository.Object);
 
             //Initialize mock ProductsController
-
             _controller = new ProductsController(mockUnitOfWork.Object);
 
         }
@@ -73,6 +72,82 @@ namespace WebApiBase.Tests
 
         }
 
+        [TestMethod]
+        public void Update_Product_ShouldReturnOK()
+        {
+
+            var product = new ProductDto
+            {
+                ProductID = 1,
+                ProductName = "Test Existing Product",
+                CategoryID = 3,
+                UnitPrice = new decimal(999.99),
+                QuantityPerUnit = "0",
+                SupplierID = 22
+            };
+
+            int id = product.ProductID;
+
+            _mockRepository.Setup(r => r.Exists(product.ProductID)).Returns(true);
+
+            var result = _controller.UpdateProduct(id, product);
+
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+
+        }
+
+        [TestMethod]
+        public void Update_Product_ShouldReturnNotFound()
+        {
+
+            var product = new ProductDto
+            {
+                ProductID = 1,
+                ProductName = "Test Existing Product",
+                CategoryID = 3,
+                UnitPrice = new decimal(999.99),
+                QuantityPerUnit = "0",
+                SupplierID = 22
+            };
+
+            int id = product.ProductID;
+
+            _mockRepository.Setup(r => r.Exists(product.ProductID)).Returns(false);
+
+            var result = _controller.UpdateProduct(id, product);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+
+        }
+
+        [TestMethod]
+        public void Delete_Product_ShouldReturnOk()
+        {
+
+            var product = new Product();
+
+            _mockRepository.Setup(p => p.GetProductById(product.ProductID)).Returns(product);
+
+            var result = _controller.DeleteProduct(product.ProductID);
+
+            //Assert.IsInstanceOfType(result, typeof(OkResult));
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+
+        }
+
+        [TestMethod]
+        public void Delete_Product_ShouldReturnNotFound()
+        {
+
+            var product = new Product();
+
+            _mockRepository.Setup(p => p.GetProductById(product.ProductID)).Returns(product);
+
+            var result = _controller.DeleteProduct(product.ProductID);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+
+        }
 
 
     }
